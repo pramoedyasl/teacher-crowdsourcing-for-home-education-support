26c79990efcd0994926d850abfbf3414964ca6d8
pramoedyasl <pramoedyasl@student.telkomuniversity.ac.id>
2022-06-01 14:22:44 +0700

    Sprint 2
    Sprint#2 - Goals : User dapat mengakses halaman profile , halaman home dan bug fix sprint 1

---
+++ application/views/profile.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css" >
+
+    <title>Profile Page</title>
+  </head>
+  <body>
+    
+
+    <div class="col-lg-5 col-lg-offset-2 " >
+    <h1>Profile Page:</h1>
+    <p>Fill in the details to Login on our website!!</p>
+<?php if(isset($_SESSION['success'])){ ?>
+    <div class="alert alert-success"><?php echo $_SESSION['success']; ?></div>
+<?php
+} ?>
+    
+    Hello,<?php echo $_SESSION['username'];?>
+
+    <br><br>
+    <a href="<?php echo base_url(); ?>index.php/auth/logout">Logout</a>
+
+    </div>
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
